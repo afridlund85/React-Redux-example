c0905c0a98dc87e07dda260a6a4d805038a81b70
Oskar Klintrot <oskar.klintrot@gmail.com>
2015-12-25 11:29:38 +0100

    Using React 0.14 stateless function components

--- src/components/count.js
+++ src/components/count.js
@@ -1,21 +1,20 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import { connect } from 'react-redux'
 import actions from '../actions'
 
-class Count extends Component {
-    render () {
-        const { currentValue, increase, decrease } = this.props
-        return (
-            <div>
-                <h2>Count</h2>
-                <p>Current count: {currentValue}</p>
-                <p>
-                    <button onClick={increase}>+</button>
-                    <button onClick={decrease}>-</button>
-                </p>
-            </div>
-        )
-    }
+const Count = (props) => {
+    const { currentValue, increase, decrease } = props
+    
+    return (
+        <div>
+            <h2>Count</h2>
+            <p>Current count: {currentValue}</p>
+            <p>
+                <button onClick={increase}>+</button>
+                <button onClick={decrease}>-</button>
+            </p>
+        </div>
+    )
 }
 
 Count.propTypes = {
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,17 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router'
 
-export default class Nav extends Component {
-    render () {
-        return (
-            <div id="nav">
-                <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/count">Count</Link></li>
-                </ul>
-                <div className="clear"/>
-            </div>
-        )
-    }
+const Nav = () => {
+    return (
+        <div id="nav">
+            <ul>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/count">Count</Link></li>
+            </ul>
+            <div className="clear"/>
+        </div>
+    )
 }
 
+export default Nav
